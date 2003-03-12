812c61de44a137703467aa6d527169b7b5783ffe
andrei <andrei>
2003-03-12 18:51:58 +0000

    - added config.h

---
+++ disc/src/diameter_api/config.h
@@ -0,0 +1,16 @@
+/*
+ * $Id: config.h,v 1.1 2003/03/12 18:51:58 andrei Exp $
+ */
+/* History:
+ * --------
+ *  2003-03-12  created by andrei
+ */
+
+#ifndef _diameter_api_config_h
+#define _diameter_api_config_h
+
+
+#define SHM_MEM_SIZE 1 /* in MB */
+
+#endif
+
