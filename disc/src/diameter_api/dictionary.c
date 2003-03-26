90dc60911200d3ff0c2ebac024ef27ddd9454ed5
ilk <ilk>
2003-03-26 17:18:45 +0000

    Some corrections in sub functions. You can use this version to get AVP entry from AVP Code or AVP Name:         AAADictionaryEntryFromAVPCode         AAADictionaryEntryFromName functions. Structure of entry is described in diameter_types.h Interface of functions in diameter_api.h This two functions were tested with test programm. Another fuctions (for VALUE and command) come later
    Illya

--- disc/src/diameter_api/dictionary.c
+++ disc/src/diameter_api/dictionary.c
@@ -1,6 +1,6 @@
 /*
  * 2003-02-17 created by illya (komarov@fokus.gmd.de)
- * $Id: dictionary.c,v 1.10 2003/03/24 19:17:54 ilk Exp $
+ * $Id: dictionary.c,v 1.11 2003/03/26 17:18:45 ilk Exp $
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,6 +58,7 @@ AAAReturnCode AAAFindDictionaryEntry(  AAAVendorId vendorId,
    char vendorName[]="ATTRIBUTE";
    char* vendorAttr=vendorName;
    char* charEntry[4];
+   int i;
       
    if(vendorId != 0){
       if(vendorNameFromId(vendorId,vendorAttr))
@@ -82,39 +83,80 @@ AAAReturnCode AAAFindDictionaryEntry(  AAAVendorId vendorId,
    entry->avpName=(char*)malloc(strlen(charEntry[1]+1));
    strcpy(entry->avpName,charEntry[1]);
    entry->avpCode=atoi(charEntry[2]);
-   //entry->avpType=
+	char* dataTypes[]={"Grouped"} ;
+   for(i=0;i<sizeof(dataTypes)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],dataTypes[i]))
+			entry->avpType=AAA_AVP_DATA_TYPE;
+   char* octetStrings[]={"OctetString","UTF8String","DiameterIdentity"} ;  
+   for(i=0;i<sizeof(octetStrings)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],octetStrings[i]))
+			entry->avpType=AAA_AVP_STRING_TYPE;
+	char* addressTypes[]={"IPAddress","DiameterURI","IPFilterRule"} ;
+   for(i=0;i<sizeof(addressTypes)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],addressTypes[i]))
+			entry->avpType=AAA_AVP_ADDRESS_TYPE;
+	char* integer32Types[]={"Interger32","Unsigned32","Enumerated"} ;
+   for(i=0;i<sizeof(integer32Types)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],integer32Types[i]))
+			entry->avpType=AAA_AVP_INTEGER32_TYPE;
+	char* integer64Types[]={"Interger64","Unsigned64"} ;
+   for(i=0;i<sizeof(integer64Types)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],integer64Types[i]))
+			entry->avpType=AAA_AVP_INTEGER64_TYPE;
+	char* timeTypes[]={"Time"} ;
+   for(i=0;i<sizeof(timeTypes)/sizeof(char*);i++)
+   	if(!strcasecmp(charEntry[4],timeTypes[i]))
+			entry->avpType=AAA_AVP_TIME_TYPE;
       return AAA_ERR_SUCCESS;
    
 
    
 }
-int findEntry( char** entry, int sizeOfEntry){
+int findEntry( char** extEntry, int sizeOfEntry){
    file=fopen("dictionary","r");
 	if(file==NULL){
       //LOG(L_ERR,"ERROR:no dictionary found!\n");
       return 0;
    }
+   int s=0;
    int match=0;
-   while(!match && findWord(entry[0])){
+   char *entry[sizeOfEntry];
+   while(!match && findWord(extEntry[0])){
       int i;
       int size=MAXWORDLEN;
       char* buf=(char*)malloc(size);
       for(i=1; i < sizeOfEntry; i++){
          size=MAXWORDLEN;
          if((size=readWord(buf,size))){
-            if(entry[i]==NULL){
-               entry[i]=(char*)malloc(size);
+            if(extEntry[i]==NULL){
+               entry[i]=(char*)malloc(size+1);
                strcpy(entry[i],buf);
                match=1;
+               //printf("%s\n",buf);
             }
             else
-            	if(strcmp(entry[i],buf)){
+            	if(strcmp(extEntry[i],buf)){
                   match=0;
                   break;
-               }   
+               }
+               else{
+               	entry[i]=(char*)malloc(size+1);
+               	strcpy(entry[i],buf);
+                	//printf("%s\n",buf);
+					}    
 			}
       }
+		free(buf);
    }
+   fclose(file);
+   if(match==1){
+		int i;
+   	for(i=0;i<sizeOfEntry;i++)
+			if(extEntry[i]==NULL){
+				extEntry[i]=(char*)malloc(strlen(entry[i])+1);
+				strcpy(extEntry[i],entry[i]);
+			}
+	}
    return match;
 }
 int findWord( char* word){
@@ -125,19 +167,19 @@ int findWord( char* word){
          while ((num=fgetc(file))!='\n' && num!=EOF);
       else{
          if(num == word[0]){
-				char buf[strlen(word)+1];
-            buf[0]=num;
+				char buff[strlen(word)+1];
+            buff[0]=num;
             int i;
             match = 1; 
             for(i=1; i<strlen(word); i++){
-               if((buf[i]=fgetc(file))=='\n' || buf[i]==EOF){
+               if((buff[i]=fgetc(file))=='\n' || buff[i]==EOF || buff[i]!= word[i]){
                   match = 0;
                   break;
                }
 					match = 1;
             }  
-            buf[strlen(word)]=0; 
-				} 
+            buff[strlen(word)]=0;
+			} 
 		}
 	}
    return match;
